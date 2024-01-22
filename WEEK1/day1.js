511622b436c049ca9917cbc9531fa3d95d67d5d2
joymacharia <machajoy23@gmail.com>
2024-01-22 13:12:18 +0300

    created day 1 concepts

---
+++ WEEK1/day1.js
@@ -0,0 +1,61 @@
+//VARIABLES
+/**
+ * @var
+ * you can redeclre a variable
+ * var name = 'Dan'
+ * var name = 'James'
+ * 
+ * you can reassign a variable
+ * var name = 'Dan'
+ * name = 'John'
+ */
+
+/**
+ * @let
+ * you can redeclare a variable
+ * let name = 'Dan'
+ * let name = 'James'
+ * 
+ * you cannot reassign a variable
+ */
+
+/**
+ * @const
+ * you cannot redeclare
+ * let name = 'Dan'
+ * 
+ * you cannot reassign
+ */
+
+// DATA TYPES
+/** Primitive Data Types
+ * @STRINGS - Characters/numbers wrapped in quotes -> 'Dan', "Dan", "123"
+ * @NUMBERS - Decimal, whole numbers, negative numbers -> "1", "1.5", "-1"
+ * @BOOLEANS - True or false
+ * @UNDEFINED - No value assigned
+ * @NULL - Nothings
+ */
+
+/**Non-primitive Data Types
+ * @LISTS - Arrays -> {1, 2, 3, 4, 5}, {"Dan", "James","John"}
+ * @OBJECTS - Dictionaries ( Key/value pairs) -> {name = "Dan"}
+ */
+
+//OPERATORS
+/**
+ * @ADDITION -> +
+ * @SUBTRACTION -> -
+ * @MULTIPLICATION -> *
+ * @DIVISION -> /
+ * @MODULUS -> %
+ */
+
+//Revenue
+//Expenses
+//Cost of goods sold
+//Profit
+
+
+let length = prompt('Length of rectangle?')
+console.log(length)
+
