f49393fac4f36aef1d1ec7658b90b3728ae1688d
joymacharia <machajoy23@gmail.com>
2024-01-29 12:38:59 +0300

    day4

--- WEEK1/day4.js
+++ WEEK1/day4.js
@@ -1,14 +1,48 @@
+// let person = {
+//     name:'John'
+//     age:'30'
+//     school:'Zindua School'
+//     personalities:['kind','humble','hardworking']
+//     hobbies:[
+//         indoor:['reading','cooking']
+//         outdoor:['swimming','hardworking']
+//     ]
 
+// }
 
-let cardata = [
-    { Car: 'Mercedes', Color: 'Grey' },
-    { Car: 'Range Rover', Color: 'Black' },
-    { Car: 'Honda', Color: 'White' },
-    { Car: 'Toyota', Color: 'Red' },
-    { Car: 'Lexus', Color: 'Aqua' }
+// console.log(person.name)
+
+// let person=[
+//     {
+//         name='alicia'
+//         age='30'
+//         city='nairobi'
+//     },
+//     {},
+//     {},
+
+// ]
+
+let person = [
+  { Name: "Alice", Age: 25, City: "New York" },
+  { Name: "Bob", Age: 30, City: "Chicago" },
+  { Name: "Charlie", Age: 35, City: "New York" },
+  { Name: "Dave", Age: 40, City: "Chicago" },
 ];
 
-let groupedByColor = groupBy(cardata, 'Color');
-console.log('Grouped By Color', groupedByColor);
+function groupBy(arr, property) {
+  let grouped = {};
+  for (let i = 0; i < arr.length; i++) {
+    let person = arr[i];
+    let key = person[property];
 
+    if (!grouped[key]) {
+      grouped[key] = [person];
+    } else {
+      grouped[key].push(person);
+    }
+  }
+  return grouped;
+}
 
+console.log(groupBy(person, "City"));
