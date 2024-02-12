e217e99e2756face387e76d59a054e16d10198b0
joymacharia <machajoy23@gmail.com>
2024-02-12 09:04:23 +0300

    weekend

--- WEEK3/day4.js
+++ WEEK3/day4.js
@@ -1,29 +1,56 @@
-const form = document.querySelector('.search-form')
-const input = document.querySelector('.search-input')
+// const input = document.querySelector('.search-input')
 const container = document.querySelector('.container')
 
 
 
 
 
-
-const url = 'https://food-recipes-with-images.p.rapidapi.com/?q=chicken%20soup';
+const url = 'https://gym-workout1.p.rapidapi.com/food';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '92311f6163msh1303da3d72b52d6p1ebf02jsnac766245dc8b',
-		'X-RapidAPI-Host': 'food-recipes-with-images.p.rapidapi.com'
+		'X-RapidAPI-Host': 'gym-workout1.p.rapidapi.com'
 	}
 };
 
-try {
-    const fetchdata = async ()=>{
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
+const fetchdata = async()=>{
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        
+        result.forEach(food => {
+            const card = document.createElement('div')
+            card.classList.add('card')
+        card.innerHTML = `
+        <h1>${food.name}</h1>
+        <div>Calories:${food.Calories}</div>
+        <div>Protein:${food.Protein}</div>
+        <div>Fat:${food.Protein}</div>
+        <div>Carbohydrates:${food.Carbonhydrates}</div>
+        <div>Weight:${food.Weight}</div>
+        `
+        container.appendChild(card)
+        })
+    } catch (error) {
+        console.error(error);
     }
+    // try {
+        
+    //     const response = await fetch(url, options);
+    //     const result = await response.json();
+    //     console.log(result)
 
-    fetchdata()
-} catch (error) {
-	console.error(error);
+    //     const card = document.createElement('div')
+    //     card.classList.add('card')
+    //     card.innerHTML = `
+    //     <h1>${result.name}</h1>
+    //     `
+    //     container.appendChild(card)
+    //     }
+    // } catch (error) {
+    //     console.error(error);
 }
+
+fetchdata()
